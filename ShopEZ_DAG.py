b86066745d27d4d79b216fea26bbc9c228e0d300
Nixon Daniel Hutahaean <nixondanielhutahaean@gmail.com>
2024-11-19 08:50:46 +0700

    Create ShopEZ_DAG.py

---
+++ ShopEZ_DAG.py
@@ -0,0 +1,63 @@
+import pendulum
+
+from airflow import models
+from airflow.models import Variable
+from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+import pytz
+
+PROJECT_ID = 'dev-nixon-daniel'
+SCHEMA = 'public'
+POSTGRES_CONNECTION_ID = 'local_postgres'
+TABLE = 'retail_data'
+GCS_BUCKET = 'shopez_landing'
+FILE_NAME = f'{TABLE}.parquet'
+    
+with models.DAG(
+    'ShopEZ_DAG',
+    description="Doing ETL from PostgreSQL to GCS",
+    start_date=pendulum.datetime(2024, 9, 30, tz="Asia/Jakarta"),
+    schedule_interval='* 1 * * *',
+    max_active_runs=1,
+    catchup=False,
+    tags=['webinar'],
+) as dag:
+
+    dump_from_postgres_to_gcs = PostgresToGCSOperator(
+        task_id='dump_from_postgres_to_gcs',
+        postgres_conn_id=POSTGRES_CONNECTION_ID,
+        sql=f"""
+        SELECT
+            *
+        FROM {SCHEMA}."{TABLE}";
+        """,
+        bucket=GCS_BUCKET,
+        filename=f'{SCHEMA}/{TABLE}/{FILE_NAME}',  # Nama file yang disimpan di GCS
+        export_format='parquet',
+        gcp_conn_id='dev_nixon_daniel',
+        approx_max_file_size_bytes=50 * 1024 * 1024, # split 50 MB
+    )
+
+    load_to_bigquery = GoogleCloudStorageToBigQueryOperator(
+        task_id='load_to_bigquery',
+        bucket=GCS_BUCKET,
+        source_objects=f'{SCHEMA}/{TABLE}/*',
+        source_format='parquet',
+        destination_project_dataset_table=f'{PROJECT_ID}.ShopEZ.{TABLE}', # Dataset harus dibuat terlebih dahulu
+        write_disposition='WRITE_TRUNCATE', # WRITE_TRUNCATE | WRITE_EMPTY
+        skip_leading_rows=1,
+        autodetect=True,
+        gcp_conn_id='dev_nixon_daniel'
+    )
+
+    show_progress = BashOperator(
+        task_id='show_progress',
+        bash_command='echo DAG finished at {{ ts_nodash }}'
+    )
+
+    # Dependency
+    dump_from_postgres_to_gcs >> load_to_bigquery >> show_progress
